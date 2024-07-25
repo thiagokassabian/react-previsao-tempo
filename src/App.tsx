0e3bbff1f7dba84510186061dd7e713a769dbda0
thiagokassabian <thiago.kassabian@gmail.com>
2024-07-25 19:12:20 -0300

    adicionado model

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,51 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import "./App.css"
 import axios from "axios"
+import { WeatherInfo } from "./model/Weather"
 
 // const apiKey = "e276bb6539f39bfb249971234d3729dd"
 
 function App() {
 	const inputRef = useRef<HTMLInputElement>(null)
+	const [city, setCity] = useState<WeatherInfo>({} as WeatherInfo)
+	const [loading, setLoading] = useState(false)
 
 	const handleClick = async () => {
-		const key = import.meta.env.VITE_APIKEY
-
 		if (inputRef.current) {
+			setLoading(true)
+			const key = import.meta.env.VITE_APIKEY
 			const city = inputRef.current.value
 			const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
 
 			const response = await axios.get(apiCall)
 			console.log(response.data)
+			setCity(response.data)
+			setLoading(false)
 		}
 	}
 
 	return (
 		<>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-			<input
-				type="text"
-				ref={inputRef}
-				className="
-				bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg 
-				focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 
-				dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-				dark:focus:ring-blue-500 dark:focus:border-blue-500"
-			/>
+			<input type="text" ref={inputRef} className="border" />
 			<button onClick={handleClick}>Buscar</button>
+
+			<div>
+				{loading ? (
+					<p>Carregando...</p>
+				) : (
+					<>
+						{city.main && (
+							<>
+								<p>Cidade: {city.name}</p>
+								<p>Temperatura: {city.main.temp}°</p>
+								<p>Umidade: {city.main.humidity}%</p>
+								<p>Velocidade do vento: {city.wind.speed} km/h</p>
+								<p>{city.weather[0].description}</p>
+							</>
+						)}
+					</>
+				)}
+			</div>
 		</>
 	)
 }
